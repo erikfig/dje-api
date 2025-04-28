428ad216dbe733de0f9caeb2c0c9ba33938fc0bb
Erik Figueiredo <erik.figueiredo@gmail.com>
2025-04-28 16:30:59 -0300

    Refatora a configuração da conexão com o banco de dados para permitir SSL dinâmico

--- src/db.ts
+++ src/db.ts
@@ -1,16 +1,25 @@
-import { Pool } from 'pg';
-
-const pool = new Pool({
-  host: process.env.POSTGRES_HOST,
-  port: Number(process.env.POSTGRES_PORT),
-  user: process.env.POSTGRES_USER,
-  password: String(process.env.POSTGRES_PASSWORD),
-  database: process.env.POSTGRES_DBNAME,
-  ssl: {
-    rejectUnauthorized: false,
-  },
-});
+import { Pool, PoolConfig } from 'pg';
 
 export const query = (text: string, params?: any[]) => {
+  const config: PoolConfig = {
+    host: process.env.POSTGRES_HOST,
+    port: Number(process.env.POSTGRES_PORT),
+    user: process.env.POSTGRES_USER,
+    password: String(process.env.POSTGRES_PASSWORD),
+    database: process.env.POSTGRES_DBNAME,
+  };
+  
+  if (process.env.POSTGRES_REJECT_UNAUTHORIZED === 'true') {
+    config.ssl = {
+      rejectUnauthorized: true,
+    };
+  } else if (process.env.POSTGRES_REJECT_UNAUTHORIZED === 'false') {
+    config.ssl = {
+      rejectUnauthorized: false,
+    };
+  }
+  
+  const pool = new Pool(config);
+
   return pool.query(text, params);
 };
